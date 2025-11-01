7a4669ee18836b3fdf8776fab5d6e0221e20864f
Soe Htet <soe_htet@attdigiverse.com>
2025-11-02 02:46:30 +1300

    ecosystem malformat fix

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,10 +1,13 @@
-export default {
+// backend/ecosystem.config.cjs
+module.exports = {
   apps: [
     {
       name: "todo-backend",
-      script: "server.js",
+      script: "src/index.js", // entry file
+      cwd: __dirname, // ensure PM2 runs from /backend
       env: {
         NODE_ENV: "production",
+        PORT: "4000",
       },
     },
   ],
